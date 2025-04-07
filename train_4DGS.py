a784e809f780692ed407ceb47c3fecd8e306b6dd
InHwan <bds06081@naver.com>
2025-04-07 20:51:08 +0900

    Update train_4DGS.py

--- train_4DGS.py
+++ train_4DGS.py
@@ -424,7 +424,7 @@ if __name__ == "__main__":
     parser.add_argument('--detect_anomaly', action='store_true', default=False)
     parser.add_argument("--test_iterations", nargs="+", type=int, default=[3000,7000,14000])
     parser.add_argument("--video_iterations", nargs="+", type=int, default=[14000, 20000, 25000, 30_000])
-    parser.add_argument("--save_iterations", nargs="+", type=int, default=[10, 14000, 20000, 25000, 30_000, 35000, 40000, 50000])
+    parser.add_argument("--save_iterations", nargs="+", type=int, default=[14000, 20000, 25000, 30_000, 35000, 40000, 50000])
     parser.add_argument("--quiet", action="store_true")
     parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[])
     parser.add_argument("--start_checkpoint", type=str, default = None)
